2a15235cbab55a929fa184a5048b70fe6e60c163
W0Ifyyy <bartosz.wilk.123@hotmail.com>
2024-11-24 23:12:01 +0100

    Quick jwt practice

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+export default {
+  development: {
+    client: "mysql",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      tableName: "migrations",
+      directory: "./src/database/migrations",
+    },
+  },
+};
